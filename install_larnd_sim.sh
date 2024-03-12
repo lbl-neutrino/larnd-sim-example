bfcb03867a8e922c171419accf64b4914a154686
Matt Kramer <mjkramer@users.noreply.github.com>
2024-03-12 14:11:02 -0700

    Use MiniRun4.5 tag of larnd-sim

--- install_larnd_sim.sh
+++ install_larnd_sim.sh
@@ -10,7 +10,7 @@ python3 -m venv larnd-sim.venv
 source larnd-sim.venv/bin/activate
 pip install --upgrade pip setuptools wheel
 
-git clone -b develop https://github.com/DUNE/larnd-sim
+git clone -b MiniRun4.5 https://github.com/DUNE/larnd-sim
 cd larnd-sim
 pip install cupy-cuda11x
 SKIP_CUPY_INSTALL=1 pip install -e .
--- run_larnd_sim.sh
+++ run_larnd_sim.sh
@@ -20,7 +20,20 @@ out_file=$(basename $in_file .hdf5 | sed 's/convert2h5/larnd/' | sed 's/.EDEPSIM
 out_dir=$SCRATCH/larnd-sim-output
 mkdir -p "$out_dir"
 
+detector_properties="larnd-sim/larndsim/detector_properties/2x2.yaml"
+pixel_layout="larnd-sim/larndsim/pixel_layouts/multi_tile_layout-2.4.16.yaml"
+response_file="larnd-sim/larndsim/bin/response_44.npy"
+light_lut_filename="/global/cfs/cdirs/dune/www/data/2x2/simulation/larndsim_data/light_LUT_M123_v1/lightLUT_M123.npz"
+light_det_noise_filename="larnd-sim/larndsim/bin/light_noise_2x2_4mod_July2023.npy"
+simulation_properties="larnd-sim/larndsim/simulation_properties/2x2_NuMI_sim.yaml"
+
 simulate_pixels.py 2x2_mod2mod_variation \
     --input_filename "$in_file" \
     --output_filename "$out_dir/$out_file" \
+    --detector_properties "$detector_properties" \
+    --pixel_layout "$pixel_layout" \
+    --response_file "$response_file" \
+    --light_lut_filename "$light_lut_filename" \
+    --light_det_noise_filename "$light_det_noise_filename" \
+    --simulation_properties "$simulation_properties" \
     --rand_seed 321
