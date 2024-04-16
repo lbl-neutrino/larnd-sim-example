46db0b473e4799135b62201018401ae9ece4193f
Matt Kramer <mjkramer@users.noreply.github.com>
2024-04-16 14:01:35 -0700

    Add example calls to nsys and ncu

--- run_larnd_sim.sh
+++ run_larnd_sim.sh
@@ -27,6 +27,25 @@ light_lut_filename="/global/cfs/cdirs/dune/www/data/2x2/simulation/larndsim_data
 light_det_noise_filename="larnd-sim/larndsim/bin/light_noise_2x2_4mod_July2023.npy"
 simulation_properties="larnd-sim/larndsim/simulation_properties/2x2_NuMI_sim.yaml"
 
+nsys=/global/common/software/dune/mkramer/misc_software/nsight-systems-2023.4.1/bin/nsys
+ncu=/global/common/software/dune/mkramer/misc_software/NVIDIA-Nsight-Compute-2024.1/ncu
+
+run_in_nsys() {
+    mkdir -p $out_dir/nsys
+    # --cudabacktrace=all gives us a crash
+    # nsys profile --cuda-memory-usage=true --cudabacktrace=all --python-backtrace=cuda --python-sampling=true \
+    $nsys profile --cuda-memory-usage=true --python-backtrace=cuda --python-sampling=true \
+        -o $out_dir/nsys/nsys-report%n "$@"
+}
+
+run_in_ncu() {
+    mkdir -p $out_dir/ncu
+    dcgmi profile --pause
+    $ncu --kernel-id :::5 -o $out_dir/ncu/ncu-report.$now "$@"
+    # $ncu -o $out_dir/ncu/ncu-report.$now "$@"
+    dcgmi profile --resume
+}
+
 simulate_pixels.py \
     --input_filename "$in_file" \
     --output_filename "$out_dir/$out_file" \
