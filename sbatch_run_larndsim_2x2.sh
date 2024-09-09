3d4bd0a7353c02bcd978b232ed517f026dc8ddde
Madan Timalsina <49295484+madantimalsina@users.noreply.github.com>
2024-09-09 13:18:37 -0700

    run sim with branch name option

--- sbatch_run_larndsim_2x2.sh
+++ sbatch_run_larndsim_2x2.sh
@@ -16,8 +16,19 @@ export desired_inputfiles_per_array
 # Calculate total tasks based on nodes and GPUs per node
 total_tasks=$(($SLURM_NNODES * $SLURM_NTASKS_PER_NODE))
 
-# Specify the branch name here or pass it via an argument when submitting
-branch_name="develop"
+# Git commands to pull the appropriate branch
+cd /pscratch/sd/m/madan12/DUNE/pre-post-benchmark/larnd-sim-example/larnd-sim
+
+# Check if the branch argument is provided
+if [ -z "$1" ]; then
+    echo "Error: No branch specified. Please provide a branch name (e.g., develop or post-hackathon2024)."
+    exit 1
+fi
+
+branch_name=$1
+git fetch origin
+git checkout $branch_name
+git pull origin $branch_name
 
 # Run the simulations across nodes and GPUs
-srun --ntasks=$total_tasks --kill-on-bad-exit=0 --cpu_bind=cores ./wrapper_run_larndsim_2x2.sh $branch_name
+srun --ntasks=$total_tasks --kill-on-bad-exit=0 --cpu_bind=cores ./wrapper_run_larndsim_2x2.sh
