a50d4c1effa4cffd036f2087c02fbc3551cbf1d5
Madan Timalsina <49295484+madantimalsina@users.noreply.github.com>
2024-09-09 11:43:07 -0700

    Install larnd-sim without specifying a branch
    Install larnd-sim without specifying a branch

--- install_larnd_sim.sh
+++ install_larnd_sim.sh
@@ -1,18 +1,27 @@
 #!/usr/bin/env bash
 
+# Unload existing modules if any
 module unload python 2>/dev/null
 module unload cudatoolkit 2>/dev/null
 
+# Load necessary modules
 module load cudatoolkit/11.7
 module load python/3.11
 
+# Create and activate a virtual environment
 python3 -m venv larnd-sim.venv
 source larnd-sim.venv/bin/activate
+
+# Install dependencies
 pip install --upgrade pip setuptools wheel
-# For validation plots:
 pip install matplotlib awkward
 
-git clone -b develop https://github.com/DUNE/larnd-sim
+# Clone larnd-sim without specifying a branch (you will switch branches later)
+if [ ! -d "larnd-sim" ]; then
+    git clone https://github.com/DUNE/larnd-sim
+fi
+
+# Install cupy-cuda11x and setup larnd-sim
 cd larnd-sim
 pip install cupy-cuda11x
 SKIP_CUPY_INSTALL=1 pip install -e .
